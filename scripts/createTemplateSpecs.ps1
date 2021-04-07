6a28014c876dd5dc37384cd4c51454f0189949e2
Michael Messina <michael.messina@appliedis.com>
2021-04-07 08:57:14 -0400

    modified powershell script to remove actual subid

--- scripts/createTemplateSpecs.ps1
+++ scripts/createTemplateSpecs.ps1
@@ -1,4 +1,5 @@
-$subId = "4611ca1e-866d-48a2-9c61-bb92d9be1fe9"
+## Define Variables
+$subId = "subIdValue"
 $tsRgName = 'template_Specs-rg'
 $location = 'eastus2'
 $templateFile = './templates/storageacct.json'
@@ -6,21 +7,21 @@ $parametersFile = './parameters/storageacct-params.json'
 $templateSpecName = 'storageSpec'
 $demoRG = 'ts_demo-rg'
 
+# Connect to correct subscription
 Set-azcontext `
     -SubscriptionId $subId
 
-#Create RGs
+# Create Resource Groups
 New-AzResourceGroup `
     -Name $tsRgName  `
     -Location $location `
     -Force
-
 New-AzResourceGroup `
     -Name $demoRG  `
     -Location $location `
     -Force
 
-#Create New Template Spec
+# Create New Template Spec
 New-AzTemplateSpec `
     -Name $templateSpecName `
     -Version '1.0b' `
@@ -29,35 +30,35 @@ New-AzTemplateSpec `
     -TemplateFile $templateFile `
     -Tag @{Dept = "AISU"; Environment = "Demo"; Project = "TemplateSpecs" }
 
-#Get Template SpecsId
+# Get Template SpecsId
 $specId = (Get-AzTemplateSpec `
         -ResourceGroupName $tsRgName `
         -Name $templateSpecName `
         -Version '1.0b').Versions.Id
 
-#Deployment using Template SpecsId Reference
+# Deployment using Template SpecsId Reference
 New-AzResourceGroupDeployment `
     -ResourceGroupName $demoRG `
     -TemplateSpecId $specId
 
-#Deploy Template SpecsId Reference with Param File
+# Deploy Template SpecsId Reference with Param File
 New-AzResourceGroupDeployment `
     -ResourceGroupName $demoRG `
     -TemplateSpecId $specId `
     -TemplateParameterFile $parametersFile `
     -Confirm
 
-#Deploy Main Template that uses a template spec.
+# Deploy Main Template that uses a template spec.
 New-AzResourceGroupDeployment `
     -ResourceGroupName $tsRgName `
     -TemplateFile .\templates\main.json `
     -Confirm
 
-#Linked Template Deployment
+## Linked Template Deployment using Template Specs
 $linkedTemplate = './templates/azuredeploy.json'
 $linkedTemplateSpecName = 'linkedTemplateSpec'
 
-#Creating Template Spec with Linked Template
+# Creating Template Spec with Linked Template
 New-AzTemplateSpec `
     -Name $linkedTemplateSpecName `
     -Version "1.0.0.2" `
@@ -66,12 +67,12 @@ New-AzTemplateSpec `
     -TemplateFile $linkedTemplate `
     -Tag @{Dept = "AISU"; Environment = "Demo"; Project = "LinkedTemplateSpecs" }
 
-#Get Id used for deployments
+# Get Id used for deployments
 $linkedTsId = (Get-AzTemplateSpec `
         -ResourceGroupName $tsRgName `
         -Name $linkedTemplateSpecName -Version "1.0.0.2").Versions.Id
 
-#Deploy using linked template specs
+# Deploy using linked template specs
 New-AzResourceGroupDeployment `
     -TemplateSpecId $linkedTsId `
     -ResourceGroupName $demoRG
